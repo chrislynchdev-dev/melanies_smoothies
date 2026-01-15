c111300f9f57daada673ae5582e3daf89c56ee51
chrislynchdev-dev <chris.lynch.dev@gmail.com>
2026-01-15 11:54:00 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,9 +13,7 @@ st.write(
 name_for_order = st.text_input('Name for smoothie:')
 st.write('The name on your smoothie will be: ', name_for_order)
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data= smoothiefroot_response.json(), use_container_width=True)
+
 cnx = st.connection('snowflake')
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
@@ -27,6 +25,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data= smoothiefroot_response.json(), use_container_width=True)
         
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
